8cc461b5c6edc790cde010d4d7c9d5524253d63f
emore23 <iurymarmore@gmail.com>
2022-03-02 08:54:46 -0300

    testando husk e lint-staged

--- apps/frontend/apps/web/src/app/app.tsx
+++ apps/frontend/apps/web/src/app/app.tsx
@@ -1,16 +1,9 @@
-import styled from 'styled-components';
-import NxWelcome from './nx-welcome';
+import React from 'react';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
-
-export function App() {
-  return (
-    <StyledApp>
-      <NxWelcome title="web" />
-    </StyledApp>
-  );
+const App: React.FC = () => {
+	return (
+	<p>hello word</p>
+		);
 }
 
 export default App;
